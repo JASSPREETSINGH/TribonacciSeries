b63100096a281a4004142f0e96502309459627ba
Dell <Dell@192.168.2.239>
2018-01-01 00:03:17 -0500

    fibonacci series converted into tribonacci

---
+++ src/tribonacci/Tribonacci.java
@@ -0,0 +1,20 @@
+/*
+fibonacci converted into tribonacci where F(n)=F(n-1)+F(n-2)+F(n-3)
+and first three numbers are 1, 1 and 2
+ */
+package tribonacci;
+
+/**
+ *
+ * @author Dell
+ */
+public class Tribonacci {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
